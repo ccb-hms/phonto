208f3125bac2539d670435ca85a963732d91c169
Robert Gentleman <rgentlem@gmail.com>
2024-01-29 10:57:20 -0500

    updates to accommodate accessing the variableMetaData - need to decide if we want it all or not - for now just pull according to supplied list of tables and variables

--- R/metadata.R
+++ R/metadata.R
@@ -9,15 +9,15 @@ meta_tables =
     )
 
 
-#' Show meta data information
+#' Retrieve meta data information from the SQL database
 #'
-#' It shows NAHANES medata such as, tables, variables and codebooks.
+#' Retrieve NHANES medata tables, variables and codebooks.
 #'
-#' @param meta meta values. If meta set as \code{T} or \code{Tables}, it shows the information of NHANES tables.
-#' If meta set as \code{V} or \code{Variables}, it shows the information of NHANES Variables.
-#' If meta set as \code{C} or \code{Codebook}, it shows the Codebook information.
+#' @param meta meta values. If meta set as \code{T} or \code{Tables}, it shows the information on NHANES tables.
+#' If meta set as \code{V} or \code{Variables}, it returns the information of NHANES Variables.
+#' If meta set as \code{C} or \code{Codebook}, it returns the Codebook information.
 #'
-#' @return dataframe contains the meta data
+#' @return dataframe containing the meta data
 #' @export
 #'
 #' @examples metaData("T")
@@ -27,3 +27,32 @@ metaData = function(meta){
   nhanesQuery(sql)
 }
 
+#' Retrieve variable meta data information from Metadata.QuestionnaireVariables
+
+#'
+#' @param varList: This is a named list, the names are valid NHANES table/questionnaire names
+#' and the elements are the names of variables.
+#'
+#' @return A named list of dataframes, the names correspond to the names from the input list
+#' and the values are dataframes that contain the information in the corresponding rows
+#' of the Metadata.QuestionnaireVariables table
+#' @export
+#'
+#' @examples input = list(AA_H = c("SEQN", "WTSA2YR", "URX2NP"),
+#'     DEMO=c("SEQN", "RIDAGEYR","RIAGENDR","RIDRETH1","DMDEDUC2", "INDFMPIR"))
+#' @examples ans = variableMetaData(input)
+#' @examples ans[[1]][1,]
+variableMetaData = function(varList) {
+  if( !is.list(varList)) stop("malformed input")
+
+  varNames = names(varList)
+  ans = vector(mode="list", length=length(varList))
+  names(ans) = varNames
+
+  for(i in 1:length(varList)) {
+    sql = paste0("SELECT * FROM Metadata.QuestionnaireVariables where
+        TableName = '", varNames[i], "'" )
+    ans[[i]] = nhanesQuery(sql)
+  }
+  return(ans)
+}
