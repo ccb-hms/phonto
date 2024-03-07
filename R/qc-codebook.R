dc4ada8d60d56b6b53a1f6e9f094d726c28856a2
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2024-03-08 01:14:07 +0530

    variable QC - updte

--- R/qc-codebook.R
+++ R/qc-codebook.R
@@ -12,25 +12,35 @@
 ## that big, or just get for specific variable? Unless we cache,
 ## second option is probably better (do once inside qc_var).
 
-.get_cb <- function() nhanesQuery("select * from Metadata.VariableCodebook")
-.get_var <- function() nhanesQuery("select * from Metadata.QuestionnaireVariables")
-.get_tab <- function() nhanesQuery("select * from Metadata.QuestionnaireDescriptions")
 
 
-
-if (FALSE)
+.where_clause <- function(variable = NULL, table = NULL)
 {
-    var <- .get_var()
-    cb <- .get_cb()
-    tab <- .get_tab()
-
-    qc_var("PHAFSTMN", var, cb, tab)
-    qc_var("LBCBHC", var, cb, tab)
-    qc_var("ENQ100", var, cb, tab)
-    qc_var("LBXHCT", var, cb, tab)
-
-
+    case <- 1L + length(variable) + 2 * length(table)
+    switch(case,
+           "",
+           sprintf("where Variable = '%s'", variable),
+           sprintf("where TableName = '%s'", table),
+           sprintf("where Variable = '%s' and TableName = '%s'", variable, table))
 }
+
+metadata_cb <- function(variable = NULL, table = NULL)
+{
+    nhanesQuery(paste("select * from Metadata.VariableCodebook",
+                      .where_clause(variable, table)))
+}
+metadata_var <- function(variable = NULL, table = NULL)
+{
+    nhanesQuery(paste("select * from Metadata.QuestionnaireVariables",
+                      .where_clause(variable, table)))
+}
+metadata_tab <- function(table = NULL)
+{
+    nhanesQuery(paste("select * from Metadata.QuestionnaireDescriptions",
+                      .where_clause(NULL, table)))
+}
+    
+
 
 ## The specific types of discrepancies we look for are:
 
@@ -55,27 +65,36 @@ qc_var_multtable <- function(x, var, cb, tab)
 
 ## - Inconsistency in Description / SasLabel (mostly benign)
 
-qc_var_description <- function(x, var, cb, tab)
+qc_var_description <- function(x, var, cb, tab, ignore.case = FALSE)
 {
     description <- subset(var, Variable == x)[["Description"]]
-    nvals <- length(unique(description))
-    nvalslc <- length(unique(tolower(description)))
-    if (nvals == 1L && nvalslc == 1L) return(NULL) # no problems
-    if (nvalslc > 1L) return(list(description_mismatch = table(description)))
-    if (nvals > 1L) return(list(description_case_mismatch = table(description)))
+    if (ignore.case) description <- tolower(description)
+    tt <- table(description)
+    if (length(tt) > 1) list(description_mismatch = table(description))
+    else NULL
 }
     
 
-qc_var_saslabel <- function(x, var, cb, tab)
+qc_var_saslabel <- function(x, var, cb, tab, ignore.case = FALSE)
 {
     saslabel <- subset(var, Variable == x)[["SasLabel"]]
-    nvals <- length(unique(saslabel))
-    nvalslc <- length(unique(trimws(tolower(saslabel))))
-    if (nvals == 1L && nvalslc == 1L) return(NULL) # no problems
-    if (nvalslc > 1L) return(list(saslabel_mismatch = table(saslabel)))
-    if (nvals > 1L) return(list(saslabel_case_mismatch = table(saslabel)))
+    if (ignore.case) saslabel <- tolower(saslabel)
+    tt <- table(saslabel)
+    if (length(tt) > 1) list(saslabel_mismatch = table(saslabel))
+    else NULL
 }
-    
+
+
+qc_var_target <- function(x, var, cb, tab, ignore.case = FALSE)
+{
+    target <- subset(var, Variable == x)[["Target"]]
+    if (ignore.case) target <- tolower(target)
+    tt <- table(target)
+    if (length(tt) > 1) list(target_mismatch = table(target))
+    else NULL
+}
+
+
 
 ## - Inconsistency in type (numeric / categorical)
 
@@ -97,10 +116,109 @@ qc_var_saslabel <- function(x, var, cb, tab)
 ## data.
 
 
-qc_var <- function(x, var = .get_var(), cb = .get_cb(), tab = .get_tab())
+
+
+
+##' QC report for a variable in NHANES
+##'
+##' .. content for \details{} ..
+##' @param x 
+##' @param var Optional data frame containing variable metadata
+##' @param cb Optional data frame containing codebook metadata
+##' @param tab Optional data frame containing table metadata
+##' @return
+##' @export
+##' @author Deepayan Sarkar
+qc_var <- function(x, var = metadata_var(x), cb = metadata_cb(x), tab = metadata_tab())
 {
-    c(qc_var_multtable(x, var, cb, tab),
-      qc_var_description(x, var, cb, tab),
-      qc_var_saslabel(x, var, cb, tab))
+    res <- c(qc_var_multtable(x, var, cb, tab),
+             qc_var_description(x, var, cb, tab),
+             qc_var_saslabel(x, var, cb, tab),
+             qc_var_target(x, var, cb, tab))
+    if (is.null(res)) res <- list()
+    structure(res,
+              variable = x,
+              class = "qc_var")
+}
+
+summary.qc_var <- function(object, ...)
+{
+    data.frame(Variable = attr(object, "variable"),
+               multtable = !is.null(object$multiple_tables),
+               description = !is.null(object$description_mismatch),
+               saslabel = !is.null(object$saslabel_mismatch),
+               target = !is.null(object$target_mismatch))
+}
+
+
+
+print.qc_var <- function(x, ...)
+{
+    ok <- TRUE
+    cat("Variable: ", attr(x, "variable"))
+    if (!is.null(x$multiple_tables))
+    {
+        ok <- FALSE
+        cat("\nAppears in multiple tables within same cycle:\n")
+        ## wcycle <- which(duplicated(x$multiple_tables$cycle))
+        ## wsub <- subset(x$multiple_tables, cycle %in% cycle[wcycle])
+        tapply(x$multiple_tables, ~ cycle, function(d) paste(d$TableName, collapse = " / ")) |>
+            array2DF(responseName = "Tables") |> print()
+    }
+    if (!is.null(x$description_mismatch))
+    {
+        ok <- FALSE
+        cat("\nMismatch in Description:\n")
+        print(array2DF(x$description_mismatch, responseName = "Frequency"))
+    }
+    if (!is.null(x$saslabel_mismatch))
+    {
+        ok <- FALSE
+        cat("\nMismatch in Saslabel:\n")
+        print(array2DF(x$saslabel_mismatch, responseName = "Frequency"))
+    }
+    if (!is.null(x$target_mismatch))
+    {
+        ok <- FALSE
+        cat("\nMismatch in Target:\n")
+        print(array2DF(x$target_mismatch, responseName = "Frequency"))
+    }
+    if (ok) cat(" --- no problems found")
+    invisible(x)
+}
+
+
+
+
+if (FALSE)
+{
+    var <- metadata_var()
+    cb <- metadata_cb()
+    tab <- metadata_tab()
+
+    qc_var("PHAFSTMN", var, cb, tab)
+    qc_var("LBCBHC", var, cb, tab)
+    qc_var("ENQ100", var, cb, tab)
+    qc_var("LBXHCT", var, cb, tab)
+
+
+    system.time({
+        var <- metadata_var()
+        cb <- metadata_cb()
+        tab <- metadata_tab()
+        qc_var("LBCBHC", var, cb, tab)
+    })
+
+    system.time(qc_var("LBCBHC"))
+
+
+    qc_var("PHAFSTMN")
+    qc_var("ENQ100")
+
+
+    qc_var("LBCBHC")
+    qc_var("LBXHCT")
+
+    
 }
 
