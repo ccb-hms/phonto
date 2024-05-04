cbfa7e49a8353f69ca490262e7e20ad24972e158
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2024-05-05 00:20:21 +0530

    add test for presence of basic DB tables

---
+++ tests/db-tests-1.R
@@ -0,0 +1,47 @@
+
+## Test that backend database has essential structures
+
+options(width = 80)
+
+library(nhanesA)
+library(phonto)
+con <- nhanesA:::cn()
+
+## allDBTables <-
+##     nhanesQuery(paste("SELECT DISTINCT TABLE_NAME",
+##                       "FROM INFORMATION_SCHEMA.TABLES", 
+##                       "WHERE TABLE_TYPE = 'BASE TABLE' AND ", 
+##                       "TABLE_CATALOG = 'NhanesLandingZone'"))
+
+## head(allDBTables[[1]])
+
+## For schema-based backends (SQL Server / Postgresql)
+
+## Tables in Metadata schema 
+MD <- c("Metadata.QuestionnaireDescriptions",
+        "Metadata.QuestionnaireVariables", 
+        "Metadata.VariableCodebook")
+
+## Selected Tables in Raw and Translated schemas
+
+NH_TABLES <- 
+    c("DEMO", "DEMO_C", "AUXAR_J", "BPX_D", "POOLTF_E", "PCBPOL_D",
+      "DRXIFF_B")
+
+RAW <- paste0("Raw.", NH_TABLES)
+TRANSLATED <- paste0("Translated.", NH_TABLES)
+
+
+if (attr(class(con), "package") == "RMariaDB") {
+    MD <- paste0("Nhanes", MD)
+    RAW <- paste0("Nhanes", RAW)
+    TRANSLATED <- paste0("Nhanes", TRANSLATED)
+}
+
+for (dbtable in c(MD, RAW, TRANSLATED)) {
+    cat("---- ", dbtable, " ----", fill = TRUE)
+    d <- DBI::dbReadTable(con, dbtable)
+    print(sort(names(d)))
+    print(dim(d))
+}
+
